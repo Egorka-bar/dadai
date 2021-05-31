90be6a6ba607a9bf05e9796f120b7dfcd8281835
egorka <kushnarenko.03@inbox.ru>
2021-05-31 19:35:06 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j;
+    cout << "Input i and j:";
+    cin >> i >> j;
+    cout << "chisla: " << i << "; " << j;
+    cout << "\ni + j = " << i + j << '\n'
+         << "i - j = " << i - j << '\n'
+         << "i * j = " << i * j << '\n'
+         << "i / j = " << i / j << '\n';
+    return 0;
+}
